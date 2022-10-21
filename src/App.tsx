179636b2254ceb118187947d8316b00af138d42f
Joseph Mawa <mj.mawa@yahoo.com>
2022-10-21 11:30:52 +0300

    initial commit with superplate

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+
+import routerProvider from "@pankod/refine-react-router-v6";
+import dataProvider from "@pankod/refine-simple-rest";
+
+function App() {
+  return (
+    <Refine
+      routerProvider={routerProvider}
+      dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+    />
+  );
+}
+
+export default App;
